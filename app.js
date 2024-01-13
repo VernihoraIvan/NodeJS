c9c5765028cd5459eebace6240d23c77fd5da623
VernihoraIvan <vernihorai@gmail.com>
2024-01-13 19:30:20 +0100

    update

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ app.use(cors());
 //   ],
 // };
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // res.header(
+//   //   "Access-Control-Allow-Headers",
+//   //   "Origin, X-Requested-With, Content-Type, Accept"
+//   // );
+//   next();
+// });
 
 // app.use(cors(corsOption));
 app.use(express.json());
